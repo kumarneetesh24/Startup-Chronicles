48a0365ec5d16bd0a8d052ad3a56f722aca675ff
Neetesh Kumar <kumarneetesh24@gmail.com>
2015-06-06 13:18:16 +0530

    deploying on heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.precompile += %w( admin.css )
 Rails.application.config.assets.precompile += %w( public.css )
 Rails.application.config.assets.precompile += %w( mypublic.js )
 Rails.application.config.assets.precompile += %w( modernizr.custom.js )
+Rails.application.config.assets.precompile += Ckeditor.assets
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
